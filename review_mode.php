83c93be7c26224cc4403d3166ce017842edcb529
nguyen.thinh.hung <nguyen.thinh.hung@framgia.com>
2017-10-26 08:27:37 +0700

    Add close button, fix styles for mobile device

--- review_mode.php
+++ review_mode.php
@@ -3,27 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="kanji-list" class="container">
     <div class="card-list">
-        <?php foreach ($words as $rowKey => $row): ?>
         <div class="row">
-            <?php foreach ($row as $colKey => $ceil): ?>
-                <div class="col">
-                    <div class="flashcard">
-                        <div class="front card">
-                            <div class="card-body">
-                                <h4><?php echo $ceil['kanji'] ?></h4>
+            <?php foreach ($words as $rowKey => $row): ?>
+                <?php foreach ($row as $colKey => $ceil): ?>
+                    <div class="flashcard-wrapper col-6 col-sm-3 col-md-2">
+                        <div class="flashcard">
+                            <div class="front card">
+                                <div class="card-body">
+                                    <button type="button" class="close" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h4><?php echo $ceil['kanji'] ?></h4>
+                                </div>
                             </div>
-                        </div>
-                        <div class="back card">
-                            <div class="card-body">
-                                <h5><?php echo $ceil['sino'] ?></h5>
-                                <p><?php echo $ceil['meaning'] ?></p>
+                            <div class="back card">
+                                <div class="card-body">
+                                    <button type="button" class="close" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h5><?php echo $ceil['sino'] ?></h5>
+                                    <p><?php echo $ceil['meaning'] ?></p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                <?php endforeach; ?>
             <?php endforeach; ?>
         </div>
-        <?php endforeach; ?>
     </div>
 </div>
 <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
